1ee0f6b5eca9ce169141256262eb0f4636117abd
sohampatil4101 <103174903+sohampatil4101@users.noreply.github.com>
2023-02-24 02:05:36 +0530

    Delete todo directory

--- todo/todoapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from todoapp.models import Contact
-from todoapp.models import Task
-
-# Register your models here.
-
-admin.site.register(Contact)
-admin.site.register(Task)
